aa8657a36401bbe3758fbf5186baedc976f5ea52
Bruno A. Padua <solucaoelogica@gmail.com>
2018-12-25 11:46:50 -0200

    projeto criado

---
+++ src/main/java/io/github/solucaologica/api/core/BusinessEmpresaApplication.java
@@ -0,0 +1,14 @@
+package io.github.solucaologica.api.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusinessEmpresaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusinessEmpresaApplication.class, args);
+	}
+
+}
+
