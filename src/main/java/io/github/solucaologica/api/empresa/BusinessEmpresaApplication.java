edb2a418c0c3c9305f4e978eab640025978d6e60
Bruno A. Padua <solucaoelogica@gmail.com>
2018-12-26 18:06:30 -0200

    Mapstruct Adicionado

--- src/main/java/io/github/solucaologica/api/empresa/BusinessEmpresaApplication.java
+++ src/main/java/io/github/solucaologica/api/empresa/BusinessEmpresaApplication.java
@@ -2,8 +2,17 @@ package io.github.solucaologica.api.empresa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+import io.github.solucaologica.api.core.mapper.EmpresaMapper;
+import io.github.solucaologica.api.empresa.controller.EmpresaController;
 
 @SpringBootApplication
+@EntityScan(basePackages = {"io.github.solucaologica.api.core.entities"})
+@ComponentScan(basePackages = {"io.github.solucaologica.api.**"})
+@EnableJpaRepositories(basePackages = {"io.github.solucaologica.api.core.repository"})
 public class BusinessEmpresaApplication {
 
 	public static void main(String[] args) {
--- src/main/java/io/github/solucaologica/api/empresa/controller/EmpresaController.java
+++ src/main/java/io/github/solucaologica/api/empresa/controller/EmpresaController.java
@@ -1,21 +1,39 @@
 package io.github.solucaologica.api.empresa.controller;
 
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.github.solucaologica.api.core.dto.EmpresaDTO;
+import io.github.solucaologica.api.core.entities.Empresa;
+import io.github.solucaologica.api.core.mapper.EmpresaMapper;
+import io.github.solucaologica.api.core.repository.EmpresaRepository;
 import io.github.solucaologica.api.core.response.Response;
 
 @RestController
 @RequestMapping("/api/empresas")
 public class EmpresaController {
 	
-	@PostMapping
-	public ResponseEntity<Response<EmpresaDTO>> cadastrar(@RequestBody EmpresaDTO empresadto) {
+	@Autowired
+	private EmpresaRepository empresaRepository;
+	
+	@Autowired
+	private EmpresaMapper empresaMapper;
+	
+	@PostMapping(value = "/create")
+	public ResponseEntity<Response<EmpresaDTO>> cadastrar(@Valid @RequestBody EmpresaDTO empresadto, BindingResult result) {
 		Response<EmpresaDTO> response = new Response<EmpresaDTO>();
+		if(result.hasErrors()) {
+			result.getAllErrors().forEach(error -> response.getErrors().add(error.getDefaultMessage()));
+			return ResponseEntity.badRequest().body(response);
+		}
+		empresaRepository.save(empresaMapper.toEntity(empresadto));
 		response.setData(empresadto);
 		return ResponseEntity.ok(response);
 	}
