249affed3171f90a7445b56e7d547c44eb3e1b47
Bruno A. Padua <solucaoelogica@gmail.com>
2019-01-10 09:15:41 -0200

    fix

--- src/main/java/io/github/solucaologica/api/empresa/BusinessEmpresaApplication.java
+++ src/main/java/io/github/solucaologica/api/empresa/BusinessEmpresaApplication.java
@@ -9,9 +9,9 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EntityScan(basePackages = {"io.github.solucaologica.api.core.entities"})
-@ComponentScan(basePackages = {"io.github.solucaologica.api.**"})
-@EnableJpaRepositories(basePackages = {"io.github.solucaologica.api.**"})
+@EntityScan(basePackages = {"io.github.solucaologica.api.core.**"})
+@ComponentScan(basePackages = {"io.github.solucaologica.api.core.**","io.github.solucaologica.api.empresa.**"})
+@EnableJpaRepositories(basePackages = {"io.github.solucaologica.api.core.**"})
 public class BusinessEmpresaApplication {
 
 	public static void main(String[] args) {
