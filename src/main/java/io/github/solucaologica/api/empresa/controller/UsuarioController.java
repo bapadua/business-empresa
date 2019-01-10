11ba5d72aefb0780315bfdd0c908c8c95c6e55f6
Bruno A. Padua <solucaoelogica@gmail.com>
2019-01-10 08:42:45 -0200

    Removido security

---
+++ src/main/java/io/github/solucaologica/api/empresa/controller/UsuarioController.java
@@ -0,0 +1,10 @@
+package io.github.solucaologica.api.empresa.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/users")
+public class UsuarioController {
+	
+}
